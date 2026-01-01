363e177aac70fcefb0f3243b7d949859fefd6cf0
LankyLuke97 <lukehowden@rocketmail.com>
2026-01-01 21:27:04 +0000

    Initial concept of rolling and getting associated hexes

--- board.py
+++ board.py
@@ -3,18 +3,24 @@ from random import shuffle
 
 class Board():
     def __init__(self):
+        chits = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
         resources = ["desert"] + ["field"]*4 + ["forest"]*4 + ["hill"]*3 + ["mountain"]*3 + ["pasture"]*4
+        shuffle(chits)
         shuffle(resources)
         rows = [3,4,5,4,3]
         self.tiles = []
+        self.probabilities = {i: [] for i in range(2, 13)}
         
         while rows:
             n = rows.pop()
             self.tiles.append([])
             for _ in range(n):
-                self.tiles[-1].append(Hex([],[],resources.pop()))
+                hex = Hex([],[],resources.pop())
+                if hex.resource_type != "desert": self.probabilities[chits.pop()].append(hex)
+                self.tiles[-1].append(hex)
         
         for row in self.tiles:
             print('-'*(len(row)*3+1))
             print(f'|{"|".join(hex.resource_type[:2] for hex in row)}|')
             print('-'*(len(row)*3+1))
+        print(self.probabilities)
--- gameplay.py
+++ gameplay.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 from board import Board
 from player import Player
 
@@ -7,3 +9,7 @@ class GameManager():
         self.players = [Player(f'Player {n}') for n in range(4)]
         for p in self.players:
             print(p)
+
+    def roll(self):
+        d1, d2 = randrange(1, 6), randrange(1, 6)
+        print(d1, d2, self.board.probabilities[d1+d2])
